e0821684b18794f2b36a58c4882eb3934f3c22f1
chjcken <chjcken123@gmail.com>
2015-07-02 16:45:27 +0700

    DB Helper implemented basically

---
+++ bchat.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+#below is my first script
+thrift --gen java -out src bchat.thrift
--- src/com/bchat/dbhelper/BChatDataBaseHelper.java
+++ src/com/bchat/dbhelper/BChatDataBaseHelper.java
@@ -4,41 +4,31 @@
  * and open the template in the editor.
  */
 package com.bchat.dbhelper;
+import com.bchat.service.Account;
+import com.bchat.service.Request;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 /**
  *
  * @author datbt
  */
 public class BChatDataBaseHelper {
+    private final String DB_DRIVER_CLASS = "com.mysql.jdbc.Driver";
+    private final String DB_URL = "jdbc:mysql://localhost/BChatDB";
+    private final String DB_USERNAME = "root";
+    private final String DB_PASSWORD = "qwe123";
+    
+    
     private Connection connect = null;
-    private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
     
-    public void readDB() throws Exception{
-        try{
-            //
-            Class.forName("com.mysql.jdbc.Driver");
-            //
-            connect = DriverManager.getConnection("jdbc:mysql://localhost/BChatDB?"
-                                                    + "user=root&password=qwe123");
-            statement = connect.createStatement();
-            
-            resultSet = statement.executeQuery("select * from BChatDB.User");
-            writeResult(resultSet);
-        }
-        catch (ClassNotFoundException | SQLException e){
-            throw e;
-        }
-        finally{
-            close();
-        }
-    }
+   
     
     private void writeResult(ResultSet resultSet) throws SQLException{
         String id, pass, name, email;
@@ -51,26 +41,123 @@ public class BChatDataBaseHelper {
         }
     }
     
+    public boolean authenticateUserLogin(String userid, String password) throws Exception{
+        boolean authenticateResult = false;
+        String sql = "select * from BChatDB.User where userid=? and password=?";
+        try{
+
+            preparedStatement = connect.prepareStatement(sql);
+            
+            preparedStatement.setString(1, userid);
+            preparedStatement.setString(2, password);
+            
+            resultSet = preparedStatement.executeQuery();
+            
+            
+            if (resultSet.last()) 
+                authenticateResult = true;
+            //if (resultSet.next())
+            //System.out.println("user: " + resultSet.getString("userid") + "\tname: " + resultSet.getString("password"));
+        }
+        catch (SQLException e){
+            throw e;
+        }
+        
+        return authenticateResult;        
+    }
+    
+    public ArrayList<Account> getFriendList(String userid) throws SQLException{
+        ArrayList<Account> friendList = new ArrayList<>();
+        String sql = "select User.* from BChatDB.User, BChatDB.Friends where Friends.userid=? and Friends.friendid=User.userid";
+        preparedStatement = connect.prepareStatement(sql);
+        //resultSet = statement.executeQuery("select User.* from BChatDB.User, BChatDB.Friends where Friends.userid=\"" + userid + "\" and Friends.friendid=User.userid");
+        preparedStatement.setString(1, userid);
+        
+        resultSet = preparedStatement.executeQuery();
+        String id, pass, name, email;
+        while (resultSet.next()){
+            id = resultSet.getString("userid");
+            pass = resultSet.getString("password");
+            name = resultSet.getString("name");
+            email = resultSet.getString("email");
+            System.out.println("User id: " + id + "\tPass: " + pass + "\tName: " + name + "\tEmail: " + email);
+            friendList.add(new Account(id, pass, name, email));
+        }
+        return friendList;
+    }
+    
+    public ArrayList<Request> getRequestList(String receiver) throws SQLException{
+        ArrayList<Request> requestList = new ArrayList<>();
+        String sql = "select * from  BChatDB.Request, BChatDB.User where Request.receiverid=? and Request.senderid=User.userid";
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setString(1, receiver);
+        
+        resultSet = preparedStatement.executeQuery();
+        String senderId, senderName, senderEmail,message;
+        int requestCode;
+        while(resultSet.next()){
+            requestCode = resultSet.getInt("requestcode");
+            senderId = resultSet.getString("userid");
+            senderName = resultSet.getString("name");
+            senderEmail = resultSet.getString("email");
+            message = resultSet.getString("message");
+            Account sender = new Account(senderId, "", senderName, senderEmail);
+            Request request = new Request(requestCode, sender, receiver, message);
+            requestList.add(request);
+            System.out.println("Code: "+requestCode+"\tSender ID: "+senderId+"\tSender name: "+senderName+"\tMessage: "+message);
+            
+        }
+        return requestList;
+    }
+    
+    public boolean insertRequest(Request request) throws SQLException{
+        boolean insertStatus = false;
+        String sql = "insert into BChatDB.Request values (default, ?, ?, ?, ?)";
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setString(1, String.valueOf(request.getRequestCode()));
+        preparedStatement.setString(2, request.getSender().getId());
+        preparedStatement.setString(3, request.getReceiver());
+        preparedStatement.setString(4, request.getMessage());
+        preparedStatement.executeUpdate();
+        return insertStatus;
+    }
+    
+    private void open(){
+        try {
+            Class.forName(DB_DRIVER_CLASS);
+          
+            connect = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println("Error at open(): " + e.getMessage());
+        }
+    }
+    
     private void close() {
     try {
       if (resultSet != null) {
         resultSet.close();
       }
 
-      if (statement != null) {
-        statement.close();
+      if (preparedStatement != null) {
+        preparedStatement.close();
       }
 
       if (connect != null) {
         connect.close();
       }
     } catch (Exception e) {
-
+        System.err.println("Error at close():" + e.getMessage());
     }
   }
     
     public static void main(String[] args) throws Exception{
         BChatDataBaseHelper dbHelper = new BChatDataBaseHelper();
-        dbHelper.readDB();
+        dbHelper.open();
+        //dbHelper.authenticateUserLogin("user1", "123456");
+        //dbHelper.getFriendList("user1");
+        //dbHelper.getRequestList("user2");
+        Request req = new Request(123, new Account("user4", "", "", ""), "user3", "insert message");
+        dbHelper.insertRequest(req);
     }
 }
